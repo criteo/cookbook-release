f0a8177a2aa45a864abd4f179f1e5e68342510f5
Dino Lukman <d.lukman@criteo.com>
2017-08-01 12:25:33 +0200

    Add rake tasks to print changelog by importance
    Gives priority to risky/breaking commits and puts them
    on top of the output. Shows a full changelog below the
    riskies without details and a detailed changelog for
    for riskies below everything.

--- lib/cookbook-release/changelog.rb
+++ lib/cookbook-release/changelog.rb
@@ -7,6 +7,11 @@ module CookbookRelease
       short_sha: true
     }
 
+    NO_RISKY = 'No risky/breaking commits.'
+    RISKY = 'Risky/Breaking (details below):'
+    FULL = 'Full changelog:'
+    DETAILS = 'Details of risky commits:'
+
     def initialize(git, opts = {})
       @git = git
       @opts = DEFAULT_OPTS.merge(opts)
@@ -14,6 +19,17 @@ module CookbookRelease
 
     def raw
       changelog.map(&:to_s_oneline)
+    end
+
+    def raw_priority
+      risky_commits = changelog.select { |c| c.risky? || c.major? }
+      result = if risky_commits.empty?
+                 "#{NO_RISKY}\n\n"
+               else
+                 "#{RISKY}\n" << risky_commits.map(&:to_s_oneline).join("\n") << "\n\n"
+               end
+      result << "#{FULL}\n"
+      result << changelog.map(&:to_s_oneline).join("\n")
     end
 
     def html
@@ -35,6 +51,31 @@ module CookbookRelease
       result.join("\n")
     end
 
+    def html_priority
+      result = []
+      risky_commits = changelog.select { |c| c.risky? || c.major? }
+      result << <<-EOH
+<html>
+  <body>
+      EOH
+      result << if risky_commits.empty?
+                  "    <p>#{NO_RISKY}</p>\n"
+                else
+                  "    <p>#{RISKY}</p>\n" << risky_commits.map { |c| c.to_s_html(false) }.map {|c| "    <p>#{c}</p>"}.join("\n")
+                end
+      result << "    <p>#{FULL}</p>\n"
+      result << changelog.map { |c| c.to_s_html(false) }.map { |c| "    <p>#{c}</p>" }.join("\n")
+      unless risky_commits.empty?
+        result << "\n<p>#{DETAILS}</p>\n"
+        result << risky_commits.map { |c| c.to_s_html(true) }.map { |c| "    <p>#{c}</p>" }.join("\n")
+      end
+      result <<  <<-EOH
+  </body>
+</html>
+      EOH
+      result.join("\n")
+    end
+
     def markdown
       changelog.map do |c|
         full_body ||= @opts[:expand_major] && c.major?
@@ -42,6 +83,22 @@ module CookbookRelease
         full_body ||= @opts[:expand_commit] && (c[:subject] =~ @opts[:expand_commit] || c[:body] =~ @opts[:expand_commit])
         c.to_s_markdown(full_body)
       end.join("\n")
+    end
+
+    def markdown_priority
+      risky_commits = changelog.select { |c| c.risky? || c.major? }
+      result = if risky_commits.empty?
+                 "*#{NO_RISKY}*\n\n"
+               else
+                 "*#{RISKY}*\n" << risky_commits.map { |c| c.to_s_markdown(false) }.join("\n") << "\n\n"
+               end
+      result << "*#{FULL}*\n"
+      result << changelog.map { |c| c.to_s_markdown(false) }.join("\n")
+      unless risky_commits.empty?
+        result << "\n\n#{DETAILS}\n"
+        result << risky_commits.map { |c| c.to_s_markdown(true) }.join("\n")
+      end
+      result
     end
 
     private
